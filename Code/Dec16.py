8c3705a1d67154674263da85973d50a5f0c54b15
Parker Tewell <parkertewell@gmail.com>
2024-12-23 17:13:08 -0500

    Slow dec16

--- Code/Dec16.py
+++ Code/Dec16.py
@@ -1,3 +1,4 @@
+from heapq import heappop, heappush
 import heapq
 with open('Input/Dec16.txt') as f:
     grid = [list(line.strip()) for line in f.readlines()]
@@ -6,7 +7,6 @@ directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def find_start_end():
-    global grid, m, n
     start, end = None, None
     for r in range(n):
         for c in range(m):
@@ -18,7 +18,6 @@ def find_start_end():
 
 
 def dijkstra(start, end):
-    global directions
     pq = [(0, start[0], start[1], 0)]  # score, r, c, d
     seen, min_score = set(), float('inf')
 
@@ -30,7 +29,7 @@ def dijkstra(start, end):
         elif (r, c, d) not in seen:
             seen.add((r, c, d))
             nr, nc = r+directions[d][0], c+directions[d][1]
-            if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] != '#':
+            if grid[nr][nc] != '#':
                 heapq.heappush(pq, (score+1, nr, nc, d))
 
             # rotate clockwise
@@ -44,5 +43,30 @@ def dijkstra(start, end):
     return min_score
 
 
+def shortest_paths(start, end):
+    # score, r, c, d, path
+    pq = [(0, start[0], start[1], 0, [(start[0], start[1])])]
+    min_score, paths = float('inf'), []
+
+    while pq:
+        score, r, c, d, path = heapq.heappop(pq)
+        if (r, c) == end:
+            if score < min_score:
+                min_score = score
+                paths = [path]
+            elif score == min_score:
+                paths.append(path)
+            continue
+
+        for i, (dr, dc) in enumerate(directions):
+            nr, nc = r+dr, c+dc
+            if grid[nr][nc] != '#' and (nr, nc) not in path and score < min_score:
+                new_path = path+[(nr, nc)]
+                new_score = score+1 if i == d else score+1000
+                heapq.heappush(pq, (new_score, nr, nc, i, new_path))
+    return paths
+
+
 start, end = find_start_end()
 print(dijkstra(start, end))
+print(len({node for path in shortest_paths(start, end) for node in path}))
