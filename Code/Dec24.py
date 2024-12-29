c43fb6bea90e86724e5892f38efc3b9c009ce95b
Parker Tewell <parkertewell@gmail.com>
2024-12-29 12:39:03 -0500

    Dec24 pt2

--- Code/Dec24.py
+++ Code/Dec24.py
@@ -9,7 +9,7 @@ ops = deque(re.findall(r'(\w+)', line) for line in lines[divider + 1:])
 op_map = {'XOR': xor, 'AND': and_, 'OR': or_}
 
 
-def pt1(wires, ops):
+def pt1(wires, ops, base=2):
     while ops:
         w1, op, w2, w3 = ops.popleft()
         if w1 in wires and w2 in wires:
@@ -17,7 +17,18 @@ def pt1(wires, ops):
         else:
             ops.append([w1, op, w2, w3])
     return int(''.join(map(lambda k: str(wires[k]), sorted(
-        [w for w in wires if w[0] == 'z'], reverse=True))), base=2)
+        [w for w in wires if w[0] == 'z'], reverse=True))), base)
 
 
 print(pt1(wires, ops))
+print(pt1(wires, ops, 10))
+
+# pt2
+# the result of x+y should equal the output of the program
+# printed both, run thru the program and just manually swap the outputs where they mismatch
+# something about a full/ half adder i have no idea its easy to just write it out
+x = int(''.join(map(lambda k: str(wires[k]), sorted(
+    [w for w in wires if w[0] == 'x'], reverse=True))), base=2)
+y = int(''.join(map(lambda k: str(wires[k]), sorted(
+    [w for w in wires if w[0] == 'y'], reverse=True))), base=2)
+print(bin(x+y)[2:])
